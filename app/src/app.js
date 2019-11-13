cd486b274db79d0668cbe55edf4bd20ef1e2f2b1
Tiago Garcia <tiagojsag@gmail.com>
2019-11-13 11:50:08 +0100

    Liveliness and readiness probes

--- app/src/app.js
+++ app/src/app.js
@@ -1,8 +1,16 @@
 const logger = require('logger');
-
-// const nock = require('nock');
-// nock.recorder.rec();
+const Koa = require('koa');
+const koaSimpleHealthCheck = require('koa-simple-healthcheck');
 
 logger.debug('Initializing doc-executor');
+
 require('services/executor-queue.service');
 require('services/status-queue.service');
+
+const app = new Koa();
+
+app.use(koaSimpleHealthCheck());
+
+const server = app.listen(process.env.PORT);
+
+module.exports = server;
