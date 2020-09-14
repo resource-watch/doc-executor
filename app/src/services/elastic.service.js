cbb4fce66d5d2cb1b550deac68f20a74196c66ea
Tiago Garcia <tiagojsag@gmail.com>
2020-09-14 10:39:18 +0200

    Revert "Merge branch 'dev' into staging"
    This reverts commit 3a80c6c732d3f8105a31ba11f4b1bb1a54a06f8d, reversing
    changes made to 688bc3ea9531ad828bf7a0129c95c0c1587133b2.

--- app/src/services/elastic.service.js
+++ app/src/services/elastic.service.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 const logger = require('logger');
 const { Client } = require('@elastic/elasticsearch');
 const config = require('config');
@@ -66,7 +67,7 @@ class ElasticService {
     }
 
     async createIndex(index, legend) {
-        logger.debug(`Creating index ${index} in elastic`);
+        logger.debug(`Creating index ${index}  in elastic`);
         const body = {
             settings: {
                 index: {
@@ -74,25 +75,27 @@ class ElasticService {
                 }
             },
             mappings: {
-                properties: {}
+                _doc: {
+                    properties: {}
+                }
             }
         };
         if (legend && legend.lat && legend.long) {
             logger.debug('Adding geo column');
-            body.mappings.properties.the_geom = {
+            body.mappings._doc.properties.the_geom = {
                 type: 'geo_shape',
                 tree: 'geohash',
                 precision: '1m',
                 points_only: true
             };
-            body.mappings.properties.the_geom_point = {
+            body.mappings._doc.properties.the_geom_point = {
                 type: 'geo_point'
             };
         }
 
         if (legend && legend.nested) {
             for (let i = 0, { length } = legend.nested; i < length; i++) {
-                body.mappings.properties[legend.nested[i]] = {
+                body.mappings._doc.properties[legend.nested[i]] = {
                     type: 'nested',
                     include_in_parent: true
                 };
@@ -117,7 +120,7 @@ class ElasticService {
         fieldTypeList.forEach((fieldType) => {
             if (legend && legend[fieldType]) {
                 for (let i = 0, { length } = legend[fieldType]; i < length; i++) {
-                    body.mappings.properties[legend[fieldType][i]] = {
+                    body.mappings._doc.properties[legend[fieldType][i]] = {
                         type: fieldType
                     };
                 }
@@ -176,10 +179,12 @@ class ElasticService {
             waitForCompletion: false,
             body: {
                 source: {
-                    index: sourceIndex
+                    index: sourceIndex,
+                    type: 'type',
                 },
                 dest: {
-                    index: destIndex
+                    index: destIndex,
+                    type: '_doc',
                 }
             }
         });
